dd35fed21a850918759a863782f9e4814718a8e6
William-Yip <whyip2016@gmail.com>
2021-04-23 00:01:30 -0300

    removing unecessary describe on tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -15,47 +15,37 @@ const makeSut = (): SutTypes => {
     }
 }
 
-describe('LocalSavePurchases', () => {
-    test('Should not delete or save cache on sut.init', () => {
-        const { cacheStore } = makeSut()
-        expect(cacheStore.actions).toEqual([])
-    })
+test('Should not delete or save cache on sut.init', () => {
+    const { cacheStore } = makeSut()
+    expect(cacheStore.actions).toEqual([])
 })
 
-describe('LocalSavePurchases', () => {
-    test('Should delete old cache on sut.save and with the given key', async () => {
-        const { cacheStore, sut } = makeSut()
-        await sut.save(mockPurchases())
-        expect(cacheStore.actions).toEqual([ CacheStoreSpy.Actions.DELETE, CacheStoreSpy.Actions.INSERT ])
-        expect(cacheStore.deleteKey).toBe('purchases')
-    })
+test('Should delete old cache on sut.save and with the given key', async () => {
+    const { cacheStore, sut } = makeSut()
+    await sut.save(mockPurchases())
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Actions.DELETE, CacheStoreSpy.Actions.INSERT])
+    expect(cacheStore.deleteKey).toBe('purchases')
 })
 
-describe('LocalSavePurchases', () => {
-    test('Should not insert new Cache data if delete fails', async () => {
-        const { cacheStore, sut } = makeSut()
-        cacheStore.simulateDeleteError()
-        const promise = sut.save(mockPurchases())
-        await expect(promise).rejects.toThrow()
-        expect(cacheStore.actions).toEqual([ CacheStoreSpy.Actions.DELETE ])
-    })
+test('Should not insert new Cache data if delete fails', async () => {
+    const { cacheStore, sut } = makeSut()
+    cacheStore.simulateDeleteError()
+    const promise = sut.save(mockPurchases())
+    await expect(promise).rejects.toThrow()
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Actions.DELETE])
 })
 
-describe('LocalSavePurchases', () => {
-    test('Should insert new Cache if delete succeeds', async () => {
-        const { cacheStore, sut } = makeSut()
-        await sut.save(mockPurchases())
-        expect(cacheStore.actions).toEqual([ CacheStoreSpy.Actions.DELETE, CacheStoreSpy.Actions.INSERT ])
-        expect(cacheStore.insertKey).toBe('purchases')
-    })
+test('Should insert new Cache if delete succeeds', async () => {
+    const { cacheStore, sut } = makeSut()
+    await sut.save(mockPurchases())
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Actions.DELETE, CacheStoreSpy.Actions.INSERT])
+    expect(cacheStore.insertKey).toBe('purchases')
 })
 
-describe('LocalSavePurchases', () => {
-    test('Should insert new Cache if delete succeeds', async () => {
-        const { cacheStore, sut } = makeSut()
-        const purchases = mockPurchases()
-        cacheStore.simulateDeleteError()
-        const promise = sut.save(purchases)
-        expect(cacheStore.actions).toEqual([ CacheStoreSpy.Actions.DELETE ])
-    })
+test('Should insert new Cache if delete succeeds', async () => {
+    const { cacheStore, sut } = makeSut()
+    const purchases = mockPurchases()
+    cacheStore.simulateDeleteError()
+    const promise = sut.save(purchases)
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Actions.DELETE])
 })
